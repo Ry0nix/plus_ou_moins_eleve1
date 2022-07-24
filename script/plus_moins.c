2aac14504f62ebb47eee5b2d6d39959c82abf8cd
Lahizarre P <109486816+Ry0nix@users.noreply.github.com>
2022-07-24 18:20:13 +0200

    v1.0

--- script/plus_moins.c
+++ script/plus_moins.c
@@ -2,7 +2,67 @@
 #include <stdlib.h>
 #include <time.h>
 
-int plus_moins(int valeur_a_trouver, int choix_joueur,char mode)
+int multi (int valeur)
+{
+    printf("Bienvenue dans le mode 2 joueurs \n");
+    int choix,coup,joueur1,joueur2,score=0,tour,phase=1;
+    joueur1=1;
+    joueur2=2;
+    printf("Pour combien de tour, souhaitez vous jouer ?: ");
+    scanf("%d",&tour);
+    for (int j=1;j<tour+1;j++)
+    {
+        printf("\nTour %d",phase);
+         printf("\nJoueur%d est maitre de jeux,quel est le nombre de coup ?: ",joueur1);
+    scanf("%d",&coup);
+    printf("Joueur%d, quel est votre choix: ",joueur2);
+    scanf("%d",&choix);
+    int count=coup;
+     for (int i=0;i<coup;i++)
+            {
+                count--;
+                if (valeur < choix)
+        {
+            printf("Moins\n");
+            printf("Il vous reste %d tentative(s)\n",count);
+        }
+        else if (valeur > choix)
+        {
+            printf("Plus\n");
+            printf("Il vous reste %d tentative(s)\n",count);
+        }
+        score=coup-i;
+     if(count==0)
+        {
+            printf("Joueur%d, vous avez %d points",joueur2,score);
+            int temp=joueur1;
+            joueur1=joueur2;
+            joueur2=temp;
+            srand(time(NULL)); 
+	valeur=rand()%100;
+	printf("%d",valeur);
+            break;
+        }
+    if (valeur == choix)
+        {
+            printf("Bingo \n");
+            printf("Joueur%d, vous avez %d points",joueur2,score);
+            int temp=joueur1;
+            joueur1=joueur2;
+            joueur2=temp;
+             srand(time(NULL)); 
+	valeur=rand()%100;
+            break;
+        }
+            printf("Tenter un autre chiffre: ");
+    scanf("%d",&choix);
+            }
+            phase=phase+1;
+    }
+   
+}
+
+int plus_moins(int valeur_a_trouver,int choix_joueur,char mode)
 {
     int count1=26;
     int count2=11;
@@ -56,10 +116,10 @@ int plus_moins(int valeur_a_trouver, int choix_joueur,char mode)
     {
         case 'd':
         {
-            for (int i=0;i<11;i++)
+            for (int i=1;i<11;i++)
             {
                 count2--;
-                printf("Tu as %d tentatives restantes\n",count2);
+                printf("Tu as %d tentative(s) restantes\n",count2);
                 if (valeur_a_trouver < choix_joueur)
                     {
                         printf("Moins\n");
@@ -87,14 +147,25 @@ int plus_moins(int valeur_a_trouver, int choix_joueur,char mode)
 }
 int main()
 {
-	int valeur,choix;
+	int valeur,nbr,choix;
 	char mode;
 	srand(time(NULL)); 
 	valeur=rand()%100;
-	printf("%d\n",valeur);
-	printf("Entrer le mode: facile(f)/moyen(m),difficile(d) ");
-	scanf("%c",&mode);
-	printf("Choisissez une chiffre : ");
-	scanf("%d",&choix);
-	plus_moins(valeur,choix,mode);
+	printf("Quel est le nombre de joueurs ?: ");
+	scanf("%d",&nbr);
+	switch(nbr)
+	{
+	    case 1:
+	    printf("\nBienvenue dans le mode 1 joueur\n");
+	    printf("\nChoisisser votre mode:\n");
+	    printf("facile(f)/moyen(m)/difficile(d): ");
+	    scanf(" %c",&mode);
+	    printf("Quel est votre chiffre ?: ");
+	    scanf("%d", &choix);
+	    plus_moins(valeur,choix,mode);
+	    break;
+	    case 2:
+	    multi(valeur);
+	    break;
+	}
 }
