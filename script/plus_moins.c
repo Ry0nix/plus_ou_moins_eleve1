9f03b53a6d3c65c172de76eb8af93cc8ec92235b
Lahizarre P <109486816+Ry0nix@users.noreply.github.com>
2022-07-24 18:17:37 +0200

    v0.2

--- script/plus_moins.c
+++ script/plus_moins.c
@@ -2,9 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 
-int plus_moins(int valeur_a_trouver, int choix_joueur)
+int plus_moins(int valeur_a_trouver, int choix_joueur,char mode)
 {
-    while (valeur_a_trouver != choix_joueur)
+    int count1=26;
+    int count2=11;
+      switch(mode)
+      {
+          case 'f':
+          printf("Vous avez des tentatives illimités\n");
+          break;
+      }
+    while (valeur_a_trouver != choix_joueur && mode=='f')
     {
         if (valeur_a_trouver < choix_joueur)
         {
@@ -14,8 +22,62 @@ int plus_moins(int valeur_a_trouver, int choix_joueur)
         {
             printf("Plus\n");
         }
-    printf("Entrer un autre chiffre: ");
+    printf("Tenter un autre chiffre: ");
     scanf("%d",&choix_joueur);
+        
+    }
+    switch (mode)
+    {
+        case 'm':
+        {
+            for (int i=0;i<26;i++)
+            {
+                count1--;
+                printf("Tu as %d tentatives restantes\n",count1);
+                if (valeur_a_trouver < choix_joueur)
+        {
+            printf("Moins\n");
+        }
+        else if (valeur_a_trouver > choix_joueur)
+        {
+            printf("Plus\n");
+        }
+        else
+        {
+            break;
+        }
+    printf("Tenter un autre chiffre: ");
+    scanf("%d",&choix_joueur);
+            }
+            
+        }
+    }
+       switch (mode)
+    {
+        case 'd':
+        {
+            for (int i=0;i<11;i++)
+            {
+                count2--;
+                printf("Tu as %d tentatives restantes\n",count2);
+                if (valeur_a_trouver < choix_joueur)
+                    {
+                        printf("Moins\n");
+                    }
+        else if (valeur_a_trouver > choix_joueur)
+        {
+            printf("Plus\n");
+        }
+        else
+        {
+            break;
+        }
+
+    printf("Tenter un autre chiffre: ");
+    scanf("%d",&choix_joueur);
+            }
+            break;
+        }
     }
     if(valeur_a_trouver == choix_joueur)
     {
@@ -26,9 +88,13 @@ int plus_moins(int valeur_a_trouver, int choix_joueur)
 int main()
 {
 	int valeur,choix;
+	char mode;
 	srand(time(NULL)); 
 	valeur=rand()%100;
+	printf("%d\n",valeur);
+	printf("Entrer le mode: facile(f)/moyen(m),difficile(d) ");
+	scanf("%c",&mode);
 	printf("Choisissez une chiffre : ");
 	scanf("%d",&choix);
-	plus_moins(valeur,choix);
+	plus_moins(valeur,choix,mode);
 }
